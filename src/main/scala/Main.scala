d4f80ed632c88f4081207a1e2ba71933c453a804
vonhyou <moi@rfung.dev>
2023-06-27 00:53:25 -0300

    remove unused imports

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,2 @@
-import com.slack.api.bolt.App
-import com.slack.api.bolt.jetty.SlackAppServer
-import com.slack.api.methods.SlackApiException
-import com.slack.api.methods.response.chat.ChatPostMessageResponse
-import com.slack.api.model.Message
-import com.slack.api.model.event.AppMentionEvent
-
-import java.io.IOException
-import scala.util.Try
-
 @main def main(): Unit =
   SlackBot.sendMessage("hello, world!")
